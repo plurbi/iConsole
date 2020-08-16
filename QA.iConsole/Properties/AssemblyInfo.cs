bb0db3fa02cd3232995c7699b59c7800c683600c
Patricio Urbieta <patricio.urbieta@gmail.com>
2020-08-15 23:25:20 -0300

    Initial commit

---
+++ QA.iConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QA.iConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QA.iConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e620363-6b2a-4ccb-9dae-e25c80168bcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QA.iConsole/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QA.iConsole
+{
+    public class TestRetenciones
+    {
+        public ComprobanteOPModel OrdenPago { get; set; }
+        public string RetencionEsperada { get; set; }
+    }
+    public class ComprobanteOPModel
+    {
+        public ComprobanteModel Comprobante { get; set; } = new ComprobanteModel();
+        public int Tipo_Pago { get; set; }
+
+    }
+    public class ComprobanteModel
+    {
+        public int Numero_Comprobante { get; set; }
+        public int ID_Comprobantes { get; set; }
+    }
+
+    [TestClass]
+    public class RetencionesArgentina
+    {
+
+        [TestMethod]
+        // La declaracion del objeto debe estar en la misma clase
+        //[DynamicData("RetencionIvaCaso1", DynamicDataSourceType.Method)]
+        //[DynamicData("RetencionIvaCaso2", DynamicDataSourceType.Method)]
+        // En una clase static
+        [DynamicData(nameof(RetencionesDataLoader.RetencionIvaCaso1), typeof(RetencionesDataLoader), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(RetencionesDataLoader.RetencionIvaCaso2), typeof(RetencionesDataLoader), DynamicDataSourceType.Method)]
+
+        public void Retencion_Must_Be(TestRetenciones opModel)
+        {
+            #region Selenium
+
+            #endregion
+
+            Assert.AreEqual(opModel.RetencionEsperada, "loquedevuelvaselenium");
+        }
+    }
+
+    public static class RetencionesDataLoader
+    {
+        public static IEnumerable<TestRetenciones[]> RetencionIvaCaso1()
+        {
+            return new[]
+            {
+                   new TestRetenciones[]
+                   {
+                      new TestRetenciones()
+                      {
+                           OrdenPago  = new ComprobanteOPModel()
+                            {
+                                Comprobante = new ComprobanteModel
+                                {
+                                    Numero_Comprobante = 1,
+                                    ID_Comprobantes = 1
+                                }                                                        ,
+                                Tipo_Pago = 2
+                            },
+                           RetencionEsperada = "loquedevuelvaselenium"
+                      }
+                   }
+            };
+        }
+        public static IEnumerable<TestRetenciones[]> RetencionIvaCaso2()
+        {
+            return new[]
+            {
+                   new TestRetenciones[]
+                   {
+                      new TestRetenciones()
+                      {
+                           OrdenPago  =   new ComprobanteOPModel()
+                            {
+                                Comprobante =  new ComprobanteModel
+                                {
+                                    Numero_Comprobante = 1,
+                                    ID_Comprobantes = 1
+                                }                                                        ,
+                                Tipo_Pago = 2
+                            },
+                           RetencionEsperada = "loquedevuelvaselenium-fail"
+                      }
+                   }
+            };
+        }
+    }
+}
